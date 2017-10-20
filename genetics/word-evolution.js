e0c40c37bce0e41a948b1c5a6e2192934833870e
Iliya <i4ko95@gmail.com>
2017-10-20 09:00:35 +0000

    non pure functions

--- genetics/word-evolution.js
+++ genetics/word-evolution.js
@@ -10,6 +10,15 @@ function range(size = 0) {
     return Array.from(Array(size).keys());
 }
 
+function maxId(array, lambda) {
+    let maxId = -1;
+    const valueArray = array.map(lambda);
+    valueArray.forEach((value, id) =>
+        maxId = valueArray[maxId] < value ? id : maxId);
+
+    return maxId;
+}
+
 
 class GeneticModel {
     constructor({
@@ -18,13 +27,36 @@ class GeneticModel {
     } = {}) {
         this.murationRate = murtationRate;
         this.populationSize = populationSize;
-        this.population = new Set();
+        this.population = [];
+        this.mostFit = {};
     }
-    
+
+    init() {
+        this.population = this.randomPopulation(this.populationSize);
+        this.mostFit = this.calculateMostFit(this.population);
+        return this;
+    }
+
+    tick() {
+        this.population = this.evolvePopulation(
+            this.population,
+            this.populationSize
+        );
+        this.mostFit = this.calculateMostFit(this.population);
+
+        return this;
+    }
+
+
+    calculateMostFit(population) {
+        const mostFitId = maxId(population, dna => this.fitness(dna));
+        return population[mostFitId];
+    }
+
     randomPopulation(size) {
         return range(size).map(() => this.randomDna());
     }
-    
+
     probabilityPick(array) {
         const arraySum = array.reduce((left, right) => left + right, 0);
         const randomPosition = random(0, arraySum);
@@ -35,24 +67,24 @@ class GeneticModel {
             return probAccumulation >= randomPosition;
         }).id || [];
     }
-    
+
     evolvePopulation(population, newPopulationSize) {
         let populationFitness = population.map(dna => this.fitness(dna));
         return range(newPopulationSize).map(() => {
             const leftParentId = this.probabilityPick(populationFitness);
             const rightParentId = this.probabilityPick(populationFitness);
 
-            return this.crossover(
+            return this.mutate(this.crossover(
                 population[leftParentId],
                 population[rightParentId]
-            );
+            ));
         });
     }
-    
+
     fitness(dna, target) {
         return dna.filter((letter, id) => letter === target[id]).length;
     }
-    
+
     randomGenome() {
         return String.fromCharCode(65 + random(0, 26));
     }
@@ -60,13 +92,13 @@ class GeneticModel {
     randomDna(size) {
         return range(size).map(() => this.randomGenome()).join('');
     }
-    
+
     crossover(leftParent, rightParent) {
         const cutPoint = Math.floor(0, leftParent.length);
         return leftParent.slice(0, cutPoint)
             .concat(rightParent.slice(cutPoint, rightParent.length));
     }
-    
+
     mutate(dna, murationRate) {
         return dna.split('').map(letter =>
             murationRate < random(0, 1) ?
